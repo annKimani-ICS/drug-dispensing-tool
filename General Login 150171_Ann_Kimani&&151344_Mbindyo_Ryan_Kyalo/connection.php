2ffb06ca38bdd815eb34b81c3ab05c3c70a9beda
annKimani-ICS <ann.wangari@strathmore.edu>
2023-06-23 16:10:05 +0300

    Login pages and and update

---
+++ General Login 150171_Ann_Kimani&&151344_Mbindyo_Ryan_Kyalo/connection.php
@@ -0,0 +1,22 @@
+<?php
+
+//OOP METHOD
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbdrugtool";
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection
+if ($conn->connect_error){
+    die("Connection failed: $conn->connect_error");
+}
+
+
+
+
+
+
+?>
